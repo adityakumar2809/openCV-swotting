34b9dc8c9abbd54c6ee40564e2ce94da887673b3
Aditya Kumar Singh <adityakumar2809@gmail.com>
2021-01-08 01:41:02 +0530

    Add screen name

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import cv2
 
 def main():
     image = cv2.imread('images/image1.jfif')
-    cv2.imshow('Cat', image)
+    cv2.imshow('Screen', image)
     cv2.waitKey(0)
 
 if __name__ == "__main__":
