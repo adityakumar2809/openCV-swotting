dd621d9b4db98f6d0a40f5a162847e6b18dc5139
Aditya Kumar Singh <adityakumar2809@gmail.com>
2021-01-08 02:50:42 +0530

    Add feature to erode images

--- basics.py
+++ basics.py
@@ -52,8 +52,34 @@ def dilateImage():
     cv2.waitKey(0)
 
 
+def erodeImage():
+    img = cv2.imread('images/image2.jfif')
+    img = cv2.Canny(
+        image=img,
+        threshold1=125,
+        threshold2=175
+    )
+    cv2.imshow('EdgeCascadeImage', img)
+
+    img = cv2.dilate(
+        src=img,
+        kernel=(7, 7),
+        iterations=5
+    )
+    cv2.imshow('DilatedImage', img)
+    
+    img = cv2.erode(
+        src=img,
+        kernel=(7, 7),
+        iterations=5
+    )
+    cv2.imshow('ErodedImage', img)
+    cv2.waitKey(0)
+
+
 def main():
-    dilateImage()
+    erodeImage()
+    # dilateImage()
     # performEdgeCascade()
     # blurImage()
     # convertToGrayScale()
