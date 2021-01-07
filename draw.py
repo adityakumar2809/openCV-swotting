30308cfc60e38e06b472360ce64fda91aebe82f0
Aditya Kumar Singh <adityakumar2809@gmail.com>
2021-01-08 02:12:11 +0530

    Make functions in draw.py

--- draw.py
+++ draw.py
@@ -2,5 +2,18 @@ import cv2
 import numpy as np
 
 
-# Create a blank canvas image
-blank = np.zeros((500, 500), dtype='uint8')
+def createBlankImage(size=(500, 500)):
+    """Create a blank canvas image"""
+    return np.zeros(size, dtype='uint8')
+
+
+def paintImage():
+    pass
+
+
+def main():
+    paintImage()
+
+
+if __name__ == "__main__":
+    main()
