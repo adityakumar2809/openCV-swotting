f00b781e1f51cceae0616e3706eb1262e42587a6
Aditya Kumar Singh <adityakumar2809@gmail.com>
2021-01-08 01:48:20 +0530

    Add function to read videos frame by frame

--- main.py
+++ main.py
@@ -7,6 +7,20 @@ def readImage():
     cv2.waitKey(0)
 
 
+def readVideo():
+    # VIDEOCAPTURE TAKES EITHER FILEPATH AS ARGUMENT OR AN INTEGER DENOTING 
+    # CAMERAS WHERE 0 IS USUALLY THE WEBCAM
+    capture = cv2.VideoCapture('videos/video1.mov')
+    while True:
+        isSuccessfullyRead, frame = capture.read()
+        cv2.imshow('VideoScreen', frame)
+
+        if cv2.waitKey(20) and 0xFF == ord('d'):
+            break
+
+    capture.release()
+    cv2.destroyAllWindows()
+
 def main():
     readImage()
 
