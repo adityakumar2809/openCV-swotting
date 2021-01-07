b1a10bc2d4ac5608e5134a9926e43a4ec283edc8
Aditya Kumar Singh <adityakumar2809@gmail.com>
2021-01-08 02:43:29 +0530

    Add feature to blur images

--- basics.py
+++ basics.py
@@ -3,13 +3,29 @@ import cv2
 
 def convertToGrayScale():
     img = cv2.imread('images/image2.jfif')
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img = cv2.cvtColor(
+        src=img,
+        code=cv2.COLOR_BGR2GRAY
+    )
     cv2.imshow('Grayscale', img)
     cv2.waitKey(0)
 
 
+def blurImage():
+    img = cv2.imread('images/image2.jfif')
+    img = cv2.GaussianBlur(
+        src=img,
+        ksize=(3, 3),
+        sigmaX=cv2.BORDER_DEFAULT
+    )
+    # KSIZE MUST BE COMPOSED OF ODD NUMBERS
+    cv2.imshow('BlurImage', img)
+    cv2.waitKey(0)
+
+
 def main():
-    convertToGrayScale()
+    blurImage()
+    # convertToGrayScale()
 
 
 if __name__ == "__main__":
