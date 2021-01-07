eda3067dd5865204beebddc255db608f7e653379
Aditya Kumar Singh <adityakumar2809@gmail.com>
2021-01-08 01:43:16 +0530

    Make separate function to read image and videos

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import cv2
 
-def main():
+
+def readImage():
     image = cv2.imread('images/image1.jfif')
     cv2.imshow('Screen', image)
     cv2.waitKey(0)
+
+
+def main():
+    readImage()
+
 
 if __name__ == "__main__":
     main()
