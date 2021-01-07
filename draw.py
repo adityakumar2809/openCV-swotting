01d4f02ef00795495944a9244586595cd3b1df93
Aditya Kumar Singh <adityakumar2809@gmail.com>
2021-01-08 02:18:10 +0530

    Paint a blank image

--- draw.py
+++ draw.py
@@ -2,13 +2,16 @@ import cv2
 import numpy as np
 
 
-def createBlankImage(size=(500, 500)):
+def createBlankImage(size=(500, 500, 3)):
     """Create a blank canvas image"""
     return np.zeros(size, dtype='uint8')
 
 
-def paintImage():
-    pass
+def paintImage(color=(0, 255, 0)):
+    blank_image = createBlankImage()
+    blank_image[:] = color
+    cv2.imshow('PaintedScreen', blank_image)
+    cv2.waitKey(0)
 
 
 def main():
