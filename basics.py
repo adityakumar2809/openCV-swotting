31cd09ad253c782f753bf72584ed850aac910420
Aditya Kumar Singh <adityakumar2809@gmail.com>
2021-01-08 02:39:58 +0530

    Add feature to convert image to grayscale

--- basics.py
+++ basics.py
@@ -1,8 +1,15 @@
 import cv2
 
 
+def convertToGrayScale():
+    img = cv2.imread('images/image2.jfif')
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    cv2.imshow('Grayscale', img)
+    cv2.waitKey(0)
+
+
 def main():
-    pass
+    convertToGrayScale()
 
 
 if __name__ == "__main__":
