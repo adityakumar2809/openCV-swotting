e3367df6d46377985d66e467d02c0ffc75d1f49e
Aditya Kumar Singh <adityakumar2809@gmail.com>
2021-01-10 23:23:48 +0530

    Add function to return blank images

--- bitwise.py
+++ bitwise.py
@@ -2,8 +2,16 @@ import cv2
 import numpy as np
 
 
+def getBlank(shape=(400, 400)):
+    blank = np.zeros(
+        shape=shape,
+        dtype='uint8'
+    )
+    return blank
+
+
 def main():
-    pass
+    blank = getBlank()
 
 
 if __name__ == "__main__":
