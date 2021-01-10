091948db3a8c5a3a5f5932f7f01720ccd8c265d6
Aditya Kumar Singh <adityakumar2809@gmail.com>
2021-01-11 00:01:20 +0530

    Get circular mask

--- histogram.py
+++ histogram.py
@@ -25,6 +25,17 @@ def getMask(img):
     return mask
 
 
+def getCircleMask(img, mask):
+    circle_mask = cv2.circle(
+        img=mask,
+        center=(img.shape[1]//2, img.shape[0]//2),
+        radius=70,
+        color=255,
+        thickness=-1
+    )
+    return circle_mask
+
+
 def convertBGRToGrayscale(img):
     gray_image = cv2.cvtColor(
         src=img,
