904960f38c4abfe40f81c1790c592548d21d1f6f
Aditya Kumar Singh <adityakumar2809@gmail.com>
2021-01-10 22:55:58 +0530

    Configure initial setup in spaces.py

---
+++ split-merge.py
@@ -0,0 +1,23 @@
+import cv2
+
+
+image_index = 0
+
+
+def getImage():
+    img = cv2.imread('images/image2.jfif')
+    return img
+
+
+def showImage(img):
+    global image_index
+    cv2.imshow(f'Image#{image_index}', img)
+    image_index += 1
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
