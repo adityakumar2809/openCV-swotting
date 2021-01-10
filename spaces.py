da9dcefba86f456af5d705c3495759525be466bc
Aditya Kumar Singh <adityakumar2809@gmail.com>
2021-01-10 22:37:36 +0530

    Add code to index the screens

--- spaces.py
+++ spaces.py
@@ -1,13 +1,18 @@
 import cv2
 
 
+image_index = 0
+
+
 def getImage():
     img = cv2.imread('images/image2.jfif')
     return img
 
 
 def showImage(img):
-    cv2.imshow('Image', img)
+    global image_index
+    cv2.imshow(f'Image#{image_index}', img)
+    image_index += 1
 
 
 def convertBGRToGrayscale(img):
