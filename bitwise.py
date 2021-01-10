9aba1d621ef3f0c8c594d3bdf3705ae9ad2483c9
Aditya Kumar Singh <adityakumar2809@gmail.com>
2021-01-10 23:27:42 +0530

    Add feature to draw rectangle

--- bitwise.py
+++ bitwise.py
@@ -2,6 +2,15 @@ import cv2
 import numpy as np
 
 
+image_index = 0
+
+
+def showImage(img):
+    global image_index
+    cv2.imshow(f'Image#{image_index}', img)
+    image_index += 1
+
+
 def getBlank(shape=(400, 400)):
     blank = np.zeros(
         shape=shape,
@@ -10,8 +19,23 @@ def getBlank(shape=(400, 400)):
     return blank
 
 
+def drawRectangle(img):
+    rectangle = cv2.rectangle(
+        img=img,
+        pt1=(30, 30),
+        pt2=(370, 370),
+        color=(255, 0, 0),
+        thickness=3
+    )
+    return rectangle
+
+
 def main():
     blank = getBlank()
+    showImage(blank)
+
+    rectangle = drawRectangle(blank.copy())
+    showImage(rectangle)
 
 
 if __name__ == "__main__":
