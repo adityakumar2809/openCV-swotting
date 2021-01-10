0bcd5dceabc24d301f2f673d081c5c0e3a9b6296
Aditya Kumar Singh <adityakumar2809@gmail.com>
2021-01-10 23:02:42 +0530

    Add feature to merge individual colors to form a BGR Image

--- split-merge.py
+++ split-merge.py
@@ -23,13 +23,24 @@ def splitColors(img):
     return b, g, r
 
 
+def mergeColors(b, g, r):
+    merged_img = cv2.merge(
+        mv=[b, g, r]
+    )
+    return merged_img
+
+
 def main():
     img = getImage()
+    showImage(img)
 
     b, g, r = splitColors(img)
     showImage(b)
     showImage(g)
     showImage(r)
+
+    merged_image = mergeColors(b, g, r)
+    showImage(merged_image)
 
     cv2.waitKey(0)
 
