05ddc08e659da80f9f306c42304398c4dcf80882
Aditya Kumar Singh <adityakumar2809@gmail.com>
2021-01-11 21:46:41 +0530

    Configure initial setup for thresh.py

---
+++ thresh.py
@@ -0,0 +1,26 @@
+import cv2
+
+
+image_index = 0
+
+
+def getImage():
+    img = cv2.imread('images/image2.jfif')
+    return img
+
+
+def showImage(img):
+    global image_index
+    cv2.imshow(f'Image#{image_index}', img)
+    image_index += 1
+
+
+def main():
+    img = getImage()
+    showImage(img)
+
+    cv2.waitKey(0)
+
+
+if __name__ == "__main__":
+    main()
